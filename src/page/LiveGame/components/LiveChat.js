8c44b8d0d305bc273aed4d7b6926356a543b9953
rraiy <raiybow@gmail.com>
2021-06-29 00:58:10 +0800

    finish basic chat room

--- src/page/LiveGame/components/LiveChat.js
+++ src/page/LiveGame/components/LiveChat.js
@@ -36,10 +36,13 @@ const ChatSpace = styled.div`
     background:rgb(38,4,64);
     height:320px;
     border-bottom:4px solid #5C2088;
-    padding:8px;
+    padding:16px;
+    overflow:scroll;
+    overflow-x:hidden;
+    word-wrap:normal;
 `
 
-const ChatType = styled.div`
+const ChatType = styled.form`
 
     padding:8px;
 
@@ -57,6 +60,12 @@ const ChatType = styled.div`
 //     width:40px;
 //     margin-right:8px;
 // `
+
+const Li = styled.li`
+    color:rgb(173,173,173);
+    margin-bottom:8px;
+    line-height:1.3em;
+`
 
 const ChatInput = styled.input`
     // opacity:1;
@@ -82,35 +91,72 @@ const ChatInput = styled.input`
 const LiveChat = () => {
 
     const [allMessages, setAllMessages] = useState([])
-    /* allMessage = {
+    const [newMessage, setNewMessage] = useState({
         text:'',
         username:'',
-        key:timestamp+uid
-        team:''
-    } */
+        key:'',
+        team:'',
+        timestamp:''  //Date.now()
+    })
+
 
     const chat_user = document.getElementById('chat_user');
     const chat_input = document.getElementById('chat_input');
     const send_btn = document.getElementById('send_btn');
     const board = document.getElementById('showMessage');
 
-    // show first entry message board
+    // show first entry message board & listener send
     useEffect(()=>{
         const texts = []
-        db.collection('chat_room/WePlay_0614_PSGLGD/messages').get().then(res => {
+        db.collection('chat_room/WePlay_0614_PSGLGD/messages').orderBy('timestamp','asc').get().then(res => {
             res.forEach(message => {
-                
-                texts.push({
-                    username: message.data().username,
-                    text: message.data().text,
-                    key:message.data().timestamp+message.data().uid,
-                    team:message.data().user_team
-                })
+                if(message.data().text){
+                    texts.push({
+                        username: message.data().username,
+                        text: message.data().text,
+                        key:message.data().timestamp, // need fix uid
+                        team:message.data().user_team
+                    })
+                }
             })
             setAllMessages(texts)
         })
+
+        // send_btn.addEventListener('click', function(e) {})
     }
     ,[])
+
+    // listen new message
+    const onTypeEvent = e => { 
+        setNewMessage({
+            username: 'Raiybow', // need fix
+            text: e.target.value,
+            key:Date.now(), // need fix uid
+            team:'', // need fix
+            timestamp:Date.now()
+        })
+
+    }
+
+    const onSendNew = e => {
+        e.preventDefault()
+        if(!newMessage.text) {return alert('空白個屁')}
+        db.collection('chat_room/WePlay_0614_PSGLGD/messages').add(newMessage)
+        .then(()=>{
+            chat_input.value = '';
+            const allTexts = [...allMessages, newMessage]
+            setAllMessages(allTexts)
+            setNewMessage({
+                text:'',
+                username:'',
+                key:'',
+                team:'',
+                timestamp:''
+            })
+        })
+        .catch(err => console.error(err))
+    }
+
 
 
     return (
@@ -123,22 +169,29 @@ const LiveChat = () => {
             <ChatDiv>
                 
                 <ChatTitle>Chat room</ChatTitle>
+
                 <ChatSpace id="showMessage" >
                     <ul>
                         {
                             allMessages ?
                             allMessages.map(list => 
                                 (
-                                    <li key={list.key}>{list.username}:{list.text}</li>
+                                    <Li key={list.key}>{list.username}：{list.text}</Li>
                                 )
-                            ) : <li>成為第一個留言者！</li>
+                            ) : <li>還沒有人發言呢！趕緊成為第一個留言者！</li>
                         }
                     </ul>
                 </ChatSpace>
-                <ChatType>
+
+                <ChatType onSubmit={onSendNew}>
                     <p id="chat_user">Username</p>
-                    <ChatInput id="chat_input" type="text" placeholder="Write a message..." />
-                    <img id="send_btn"  src={iconSend} alt="send" />
+                    <ChatInput 
+                        id="chat_input" 
+                        onChange={e => onTypeEvent(e)}
+                        value={newMessage.text}
+                        type="text" 
+                        placeholder="Write a message..." />
+                    <button type="submit"><img id="send_btn"  src={iconSend} alt="send" /></button>
                 </ChatType>
 
             </ChatDiv>
