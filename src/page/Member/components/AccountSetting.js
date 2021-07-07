afcb961559543c1cecac5384d11573d4971a0093
rraiy <raiybow@gmail.com>
2021-07-08 03:34:46 +0800

    AccountSetting save and cancel func finish (need connect db)

--- src/page/Member/components/AccountSetting.js
+++ src/page/Member/components/AccountSetting.js
@@ -1,21 +1,26 @@
 import React, {useState, useEffect} from 'react';
+import {db} from '../../../firebase/firestore';
 import {AccountWrap, H1, TitleDiv, Li, UL, EditDiv, Input, Button} from './css/AccountSettingSty';
 import MiniI from '../../../images/icon/minimize.png'
 
 
 
 const AccountSetting = ({user, userToken}) => {
-    const [username, setUsername] = useState(user);
+    const [username, setUsername] = useState('ye');
     const [email, setEmail] = useState('raiy@gmail');
-    const [password, setPassword] = useState(null);
+    const [password, setPassword] = useState('5566');
     const [birthday, setBirthday] =useState('2020-01-01');
 
     const [activeItem, setActiveItem] = useState([]);
 
+    const [newUsername, setNewUsername] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [newBirthday, setNewBirthday] =useState('');
+
     const AccountSettingItems = [
         {
             title:'Username',
-            value:"Raiy"
+            value:username
         },
         {
             title:'Email Address',
@@ -23,7 +28,7 @@ const AccountSetting = ({user, userToken}) => {
         },
         {
             title:'Password',
-            value:'*********'
+            value:password
         },
         {
             title:'Birthday',
@@ -44,24 +49,56 @@ const AccountSetting = ({user, userToken}) => {
     const renderItem = AccountSettingItems.map((item,index)=>{
         const active = activeItem.includes(item.title) ? 'active':'';
         return(<Li key={item.title} >
-            <img src={MiniI} alt="edit" onClick={()=>openEdit(item.title)}/>
+            {
+                item.title === 'Email Address' ? null:<img src={MiniI} alt="edit" onClick={()=>openEdit(item.title)}/>
+            }
+            
             <TitleDiv>
                 <h3>{item.title}</h3>
                 <p>{item.value}</p>
             </TitleDiv>
             <EditDiv className={`edit ${active}`}>
                 <p>Update your {item.title}:</p>
-                <Input type="text" id={item.title} />
-                <Button className='save'>Save</Button>
-                <Button className='cancel'>Cancel</Button>
+                <Input type="text"
+                    onChange={e => handleInputChange(e.target.value, index)}
+                    value={item.title === 'Username' ? newUsername: item.title === 'Password' ? newPassword:newBirthday}
+                />
+                <Button className='save' onClick={()=>onSaveValue(index)}>Save</Button>
+                <Button className='cancel' onClick={()=>onCancelValue(index)}>Cancel</Button>
             </EditDiv>
             
         </Li>)
-    })
+    });
 
+    const handleInputChange = (newValue, index) => {
+        switch(index){
+            case 0: setNewUsername(newValue);
+            break;
+            case 2: setNewPassword(newValue);
+            break;
+            case 3: setNewBirthday(newValue);
+        }
+    }
 
+    const onSaveValue = (index) => {
+        switch(index){
+            case 0: setUsername(newUsername);
+            break;
+            case 2: setPassword(newPassword);
+            break;
+            case 3: setBirthday(newBirthday);
+        }
+    }
 
-
+    const onCancelValue = (index) => {
+        switch(index){
+            case 0: setNewUsername('');
+            break;
+            case 2: setNewPassword('');
+            break;
+            case 3: setNewBirthday('');
+        }
+    }
 
     return(
         <AccountWrap>
